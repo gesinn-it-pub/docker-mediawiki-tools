d239522b1230f1a8fd7cab2edb74122686bd7a27
Alexander <gesinn-it-gea@gesinn.it>
2023-04-27 16:48:41 +0200

    added COMPOSER_AUTH

--- build-tools/composer-require.sh
+++ build-tools/composer-require.sh
@@ -3,4 +3,4 @@
 PACKAGE=$1
 VERSION=$2
 
-COMPOSER=composer.local.json composer require --no-update "$PACKAGE" "$VERSION"
+COMPOSER_AUTH="{\"github-oauth\": {\"github.com\": \"$(cat /run/secrets/GH_API_TOKEN)\"}}" COMPOSER=composer.local.json composer require --no-update "$PACKAGE" "$VERSION"
