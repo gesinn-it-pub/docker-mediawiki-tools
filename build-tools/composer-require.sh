3ba4c05572ed97275cbe0f4bbd0bac0e38700d76
gesinn-it-gea <gesinn-it-gea@gesinn.it>
2023-05-22 11:20:01 +0200

    Only use COMPOSER_AUTH environment variable, if a token secret is available.

--- build-tools/composer-require.sh
+++ build-tools/composer-require.sh
@@ -3,4 +3,10 @@
 PACKAGE=$1
 VERSION=$2
 
-COMPOSER_AUTH="{\"github-oauth\": {\"github.com\": \"$(cat /run/secrets/GH_API_TOKEN 2>/dev/null || echo no_token)\"}}" COMPOSER=composer.local.json composer require --no-update "$PACKAGE" "$VERSION"
+# Only use COMPOSER_AUTH environment variable, if a token secret is available.
+# Setting the environment variable inline with the command avoids to persist beyond that command.
+if [ -f /run/secrets/GH_API_TOKEN ]; then
+    COMPOSER_AUTH="{\"github-oauth\": {\"github.com\": \"$(cat /run/secrets/GH_API_TOKEN 2>/dev/null || echo no_token)\"}}" COMPOSER=composer.local.json composer require --no-update "$PACKAGE" "$VERSION"
+else
+    COMPOSER=composer.local.json composer require --no-update "$PACKAGE" "$VERSION"
+fi
--- build-tools/composer-update.sh
+++ build-tools/composer-update.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 sudo -u www-data COMPOSER_AUTH="{\"github-oauth\": {\"github.com\": \"$(cat /run/secrets/GH_API_TOKEN 2>/dev/null || echo no_token)\"}}" composer update --prefer-source
+
+# Only use COMPOSER_AUTH environment variable, if a token secret is available.
+# Setting the environment variable inline with the command avoids to persist beyond that command.
+if [ -f /run/secrets/GH_API_TOKEN ]; then
+    sudo -u www-data COMPOSER_AUTH="{\"github-oauth\": {\"github.com\": \"$(cat /run/secrets/GH_API_TOKEN 2>/dev/null || echo no_token)\"}}" composer update --prefer-source
+else
+    sudo -u www-data composer update --prefer-source
+fi
