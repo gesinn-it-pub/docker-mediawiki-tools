5611c745d9894447f14f3fce6551e76ec7f46975
gesinn-it-gea <gesinn-it-gea@gesinn.it>
2023-05-22 10:52:53 +0200

    return a dummy string instead of empty string, if no token has been found

--- build-tools/composer-update.sh
+++ build-tools/composer-update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo -u www-data COMPOSER_AUTH="{\"github-oauth\": {\"github.com\": \"$(cat /run/secrets/GH_API_TOKEN 2>/dev/null || true)\"}}" composer update --prefer-source
+sudo -u www-data COMPOSER_AUTH="{\"github-oauth\": {\"github.com\": \"$(cat /run/secrets/GH_API_TOKEN 2>/dev/null || echo no_token)\"}}" composer update --prefer-source
