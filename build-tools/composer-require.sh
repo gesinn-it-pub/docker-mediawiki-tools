f2bfdf23ebed70779727ba39c0bb046cc4a57b59
gesinn-it-gea <gesinn-it-gea@gesinn.it>
2023-05-22 10:34:18 +0200

    attempt to ignore error when GH_API_TOKEN is not set

--- build-tools/composer-require.sh
+++ build-tools/composer-require.sh
@@ -3,4 +3,4 @@
 PACKAGE=$1
 VERSION=$2
 
-COMPOSER_AUTH="{\"github-oauth\": {\"github.com\": \"$(cat /run/secrets/GH_API_TOKEN)\"}}" COMPOSER=composer.local.json composer require --no-update "$PACKAGE" "$VERSION"
+COMPOSER_AUTH="{\"github-oauth\": {\"github.com\": \"$(cat /run/secrets/GH_API_TOKEN 2>/dev/null || true)\"}}" COMPOSER=composer.local.json composer require --no-update "$PACKAGE" "$VERSION"
