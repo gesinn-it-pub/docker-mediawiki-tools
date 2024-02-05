003a62fc6c0680002a1a02a5cabe4feb52805cfb
Luke Eversfield <luke.eversfield@gesinn.it>
2024-02-05 15:15:56 +0100

    prepared 1.8.1 add xdebug /vscode related files

--- tools/enable-xdebug.sh
+++ tools/enable-xdebug.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# copy needed files
+cp -r ./files/.vscode /var/www/html/
+cp -r ./files/php/conf.d/99-xdebug.ini /usr/local/etc/php/conf.d/99-xdebug.ini
+
+# install and activate xdebug
+pecl install xdebug-3.1.6
 php-enable.sh /usr/local/etc/php/conf.d/99-xdebug.ini
