d5d7009dbb8b247c162c91c241bf6b42c0142497
gesinn-it-gea <gesinn-it-gea@gesinn.it>
2024-07-31 14:11:21 +0200

    chg (initialize-wiki): BREAKING. Change path from "extensions/CirrusSearch" to "extensions/Cirrussearch" required for Cirrus MW 1.39+

--- tools/initialize-wiki.sh
+++ tools/initialize-wiki.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 ELASTICSEARCH_HOST=${ELASTICSEARCH_HOST:-}
 
 setup_local_settings() {
-    echo "Setting up LocalSettings.php"
+    echo "--- Setting up LocalSettings.php ---"
 
     echo '$wgNetworkEnvironment = [];' >> LocalSettings.php;
     if [ "$ELASTICSEARCH_HOST" != "" ]; then
@@ -15,23 +15,24 @@ setup_local_settings() {
     echo 'if (file_exists( "$IP/LocalSettings.TMP.php" )) require_once( "$IP/LocalSettings.TMP.php" );' >> LocalSettings.php
 
     sudo -u www-data php maintenance/update.php --skip-external-dependencies --quick
+    echo "=== Setting up LocalSettings.php ==="
 }
 
 initialize_cirrus() {
     if [ "$ELASTICSEARCH_HOST" != "" ]; then
-        echo "Initializing Cirrus Search"
+        echo "--- Initializing Cirrus Search ---"
         echo "Waiting for elasticsearch server to be ready..."
         wait-for-it.sh -h $ELASTICSEARCH_HOST -p 9200 -t 60
         echo '<?php $wgDisableSearchUpdate = true; echo "*** Inside TMP ***\n"; ' >> LocalSettings.TMP.php
-        php extensions/CirrusSearch/maintenance/UpdateSearchIndexConfig.php && \
+        php extensions/Cirrussearch/maintenance/UpdateSearchIndexConfig.php && \
         rm LocalSettings.TMP.php && \
-        php extensions/CirrusSearch/maintenance/ForceSearchIndex.php --skipLinks --indexOnSkip && \
-        php extensions/CirrusSearch/maintenance/ForceSearchIndex.php --skipParse
+        php extensions/Cirrussearch/maintenance/ForceSearchIndex.php --skipLinks --indexOnSkip && \
+        php extensions/Cirrussearch/maintenance/ForceSearchIndex.php --skipParse
     fi
 }
 
 save_settings() {
-    echo "Saving LocalSettings for later"
+    echo "--- Saving LocalSettings for later ---"
     save-wiki-settings.sh
 }
 
@@ -48,23 +49,23 @@ setup_cron_job() {
 
 if [ -e LocalSettings.php ]; then
     # Case 1: The container has already been started before
-    echo "LocalSettings.php exists. Nothing to do."
+    echo ">>> LocalSettings.php exists. Nothing to do."
 else
     # Case 2: The container is starting the first time after creation from the image
-    echo "LocalSettings.php is missing."
+    echo ">>> LocalSettings.php is missing."
 
     # Ensure the log folder exists
-    mkdir -p /data/log && chown www-data. /data/log
+    mkdir -p /data/log && chown www-data:www-data /data/log
 
     configure-container.sh
 
     if [ -e /data/LocalSettings.php ]; then
         # Case 2a: there has already been a running container for this environment
-        echo "Settings in /data exist. Taking them."
+        echo ">>> Settings in /data exist. Taking them."
         restore_settings
     else
         # Case 2a: this is a completely fresh environment which has to be initialized first
-        echo "/data/LocalSettings.php missing, too. Need to create one."
+        echo ">>> /data/LocalSettings.php missing, too. Need to create one."
         install-mediawiki.sh
         setup_local_settings
         initialize_cirrus
