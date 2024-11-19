6a0d2963b55d493facdbd3c33dccdf1b9c0ca21b
gesinn-it-ilm <gesinn-it-ilm@gesinn.it>
2024-11-19 14:09:32 +0100

    update (tools): add check for xDebug installation status in prepare-dev.sh
    - check the installed version of xDebug, install the required version 3.1.6
    - this is important to run prepare-dev and install xDebug inside extension itself

--- tools/prepare-dev.sh
+++ tools/prepare-dev.sh
@@ -3,7 +3,28 @@
 # copy needed vscode files
 cp -r /tools/files/.vscode /var/www/html/
 
-# install and activate xdebug
-pecl install xdebug-3.1.6
+# Uninstall Xdebug if it is installed
+installed_version=$(php -r 'echo phpversion("xdebug");' 2>/dev/null || echo "not_installed")
+
+if [ "$installed_version" != "not_installed" ]; then
+    echo "Xdebug is installed. Uninstalling it first..."
+    pecl uninstall xdebug
+else
+    echo "Xdebug is not installed, proceeding with installation."
+fi
+
+# Install and activate Xdebug
+required_version="3.1.6"
+installed_version=$(php -r 'echo phpversion("xdebug");' 2>/dev/null || echo "not_installed")
+
+if [ "$installed_version" = "not_installed" ]; then
+    echo "Xdebug not installed. Installing version $required_version."
+    pecl install xdebug-$required_version
+elif [ "$(printf '%s\n' "$required_version" "$installed_version" | sort -V | head -n1)" != "$required_version" ]; then
+    echo "Xdebug version is older than $required_version. Updating."
+    pecl install xdebug-$required_version
+else
+    echo "Xdebug is already installed and meets the required version ($installed_version)."
+fi
 
 enable-xdebug.sh
