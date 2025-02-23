83f7c62874277feabf3df04fda69e23fef40c2ae
gesinn-it-gea <gesinn-it-gea@gesinn.it>
2025-02-23 17:36:38 +0100

    chg (composer): using Composer with the "no-cache" option during Docker image builds minimizes disk usage and ensures a cleaner, more reproducible final image by avoiding unnecessary cached files.

--- build-tools/composer-require.sh
+++ build-tools/composer-require.sh
@@ -6,7 +6,7 @@ VERSION=$2
 # Only use COMPOSER_AUTH environment variable, if a token secret is available.
 # Setting the environment variable inline with the command avoids to persist beyond that command.
 if [ -f /run/secrets/GH_API_TOKEN ]; then
-    COMPOSER_AUTH="{\"github-oauth\": {\"github.com\": \"$(cat /run/secrets/GH_API_TOKEN 2>/dev/null || echo no_token)\"}}" COMPOSER=composer.local.json composer require --no-update "$PACKAGE" "$VERSION"
+    COMPOSER_AUTH="{\"github-oauth\": {\"github.com\": \"$(cat /run/secrets/GH_API_TOKEN 2>/dev/null || echo no_token)\"}}" COMPOSER=composer.local.json composer require --no-cache --no-update "$PACKAGE" "$VERSION"
 else
-    COMPOSER=composer.local.json composer require --no-update "$PACKAGE" "$VERSION"
+    COMPOSER=composer.local.json composer require --no-cache --no-update "$PACKAGE" "$VERSION"
 fi
--- build-tools/composer-update.sh
+++ build-tools/composer-update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-COMPOSER_AUTH="{\"github-oauth\": {\"github.com\": \"$(cat /run/secrets/GH_API_TOKEN 2>/dev/null || echo no_token)\"}}" composer update --prefer-source
+COMPOSER_AUTH="{\"github-oauth\": {\"github.com\": \"$(cat /run/secrets/GH_API_TOKEN 2>/dev/null || echo no_token)\"}}" composer update --no-cache --prefer-source
 
 # Only use COMPOSER_AUTH environment variable, if a token secret is available.
 # Setting the environment variable inline with the command avoids to persist beyond that command.
 if [ -f /run/secrets/GH_API_TOKEN ]; then
-    COMPOSER_AUTH="{\"github-oauth\": {\"github.com\": \"$(cat /run/secrets/GH_API_TOKEN 2>/dev/null || echo no_token)\"}}" composer update --prefer-source
+    COMPOSER_AUTH="{\"github-oauth\": {\"github.com\": \"$(cat /run/secrets/GH_API_TOKEN 2>/dev/null || echo no_token)\"}}" composer update --no-cache --prefer-source
 else
-    composer update --prefer-source
+    composer update --no-cache --prefer-source
 fi
