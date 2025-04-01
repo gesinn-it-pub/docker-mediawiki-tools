2f25f10a951935a4a825beb874257b7950e4508d
gesinn-it-gea <gesinn-it-gea@gesinn.it>
2025-04-01 16:29:00 +0200

    chg (composer-require): allow optional third parameter to pass in extra config parameters

--- build-tools/composer-require.sh
+++ build-tools/composer-require.sh
@@ -2,11 +2,19 @@
 
 PACKAGE=$1
 VERSION=$2
+CONFIG_OVERRIDE=${3:-}
 
-# Only use COMPOSER_AUTH environment variable, if a token secret is available.
-# Setting the environment variable inline with the command avoids to persist beyond that command.
+# If a config override is provided, pass it directly to composer config.
+# For example, you might pass:
+#   repositories.optional '{"type": "vcs", "url": "https://github.com/SemanticMediaWiki/SemanticResultFormats"}'
+if [ -n "$CONFIG_OVERRIDE" ]; then
+    composer config $CONFIG_OVERRIDE
+fi
+
+# Only use COMPOSER_AUTH environment variable if a token secret is available.
 if [ -f /run/secrets/GH_API_TOKEN ]; then
-    COMPOSER_AUTH="{\"github-oauth\": {\"github.com\": \"$(cat /run/secrets/GH_API_TOKEN 2>/dev/null || echo no_token)\"}}" COMPOSER=composer.local.json composer require --no-cache --no-update "$PACKAGE" "$VERSION"
+    COMPOSER_AUTH="{\"github-oauth\":{\"github.com\":\"$(cat /run/secrets/GH_API_TOKEN 2>/dev/null || echo no_token)\"}}" \
+    COMPOSER=composer.local.json composer require --no-cache --no-update "$PACKAGE" "$VERSION"
 else
     COMPOSER=composer.local.json composer require --no-cache --no-update "$PACKAGE" "$VERSION"
 fi
